076e05c90a9344c5ce88641c83ec64010b7ff884
rmak-naz <40699936+rmak-naz@users.noreply.github.com>
2018-11-29 19:01:33 -0700

    Add files via upload

---
+++ ICA12_ExtensionMethods_Console/ICA12_Class.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICA12_ExtensionMethods_Console
+{
+    static class ICA12_Class
+    {
+        //This extension only applies to int lists
+        /*
+        public static Dictionary<int, int> Categorize(this List<int> sourcelist)
+        {
+            Dictionary<int, int> newDic = new Dictionary<int, int>();
+
+            sourcelist.ForEach(item =>
+            {
+                if (newDic.ContainsKey(item))
+                    newDic[item] += 1;
+                else
+                    newDic.Add(item, 1);
+            });
+
+            return newDic;
+        }
+        */
+
+        //This extension is limited to only list collections
+        /*
+        public static Dictionary<T, int> Categorize<T>(this List<T> sourcelist)
+        {
+            Dictionary<T, int> newDic = new Dictionary<T, int>();
+
+            sourcelist.ForEach(item =>
+            {
+                if (newDic.ContainsKey(item))
+                    newDic[item] += 1;
+                else
+                    newDic.Add(item, 1);
+            });
+
+            newDic = newDic.OrderBy(item => item.Key).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+
+            return newDic;
+        }
+        */
+
+        public static Dictionary<K, int> Categorize<K>(this IEnumerable<K> sourcelist)
+        {
+            Dictionary<K, int> newDic = new Dictionary<K, int>();
+
+            foreach (var item in sourcelist)
+            {
+                if (newDic.ContainsKey(item))
+                    newDic[item] += 1;
+                else
+                    newDic.Add(item, 1);
+            }
+
+            newDic = newDic.OrderBy(item => item.Key).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+
+            return newDic;
+        }
+
+        public static K Popular<K>(this IEnumerable<K> sourcelist)
+        {
+            Dictionary<K, int> newDic = new Dictionary<K, int>();
+
+            foreach (var item in sourcelist)
+            {
+                if (newDic.ContainsKey(item))
+                    newDic[item] += 1;
+                else
+                    newDic.Add(item, 1);
+            }
+
+            KeyValuePair<K, int> maxVal = newDic.Aggregate((a, b) => a.Value > b.Value ? a : b);
+            //newDic = newDic.OrderByDescending(item => item.Value).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+
+            return newDic.First().Key;
+        }
+
+        public static IEnumerable<K> Shuffle<K>(this IEnumerable<K> sourcelist, Random rnd)
+        {
+            List<K> shuffledList = new List<K>(sourcelist);
+            for (int i = 0; i < shuffledList.Count(); ++i)
+            {
+                int j = rnd.Next(i, shuffledList.Count());
+                K temp = shuffledList[j];
+                shuffledList[j] = shuffledList[i];
+                shuffledList[i] = temp;
+            }
+
+            return shuffledList;
+        }
+    }
+}
---
+++ ICA12_ExtensionMethods_Console/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICA12_ExtensionMethods_Console
+{
+    class Program
+    {
+        static Random _rnd = new Random();
+        static void Main(string[] args)
+        {
+            List<int> iNums = new List<int>(new int[] { 4, 12, 4, 3, 5, 6, 7, 6, 12 });
+            foreach (KeyValuePair<int, int> scan in iNums.Categorize())
+                Console.WriteLine(scan.Key.ToString("d3") + " x " + scan.Value.ToString("d5"));
+
+            Console.WriteLine("------------");
+            List<string> names = new List<string>(new string[] {
+                "Rick", "Glenn", "Rick", "Carl", "Michonne", "Rick", "Glenn" });
+            foreach (KeyValuePair<string, int> scan in names.Categorize())
+                Console.WriteLine(scan.Key + " x " + scan.Value.ToString("d5"));
+
+            Console.WriteLine("------------");
+            LinkedList<char> llfloats = new LinkedList<char>();
+            while (llfloats.Count < 1000)
+                llfloats.AddLast((char)_rnd.Next('A', 'Z' + 1));
+            foreach (KeyValuePair<char, int> scan in llfloats.Categorize())
+                Console.WriteLine(scan.Key + " x " + scan.Value.ToString("d5"));
+
+            Console.WriteLine("------------");
+            string TestString = "This is the test string, do not panic!";
+            foreach (KeyValuePair<char, int> scan in TestString.Categorize())
+                Console.WriteLine(scan.Key + " x " + scan.Value.ToString("d5"));
+
+            Console.WriteLine("------------");            
+            Console.WriteLine("Popular() test.");
+            Console.WriteLine(TestString);
+            Console.WriteLine(TestString.Popular() + " is the most popular value.");
+
+            foreach (int scan in iNums)
+                Console.Write(scan + " ");
+            Console.WriteLine();
+            Console.WriteLine(iNums.Popular() + " is the most popular int value.");
+
+            
+
+            Console.WriteLine("------------");
+            Console.WriteLine("Shuffle() test.");
+            foreach (char scan in TestString.Shuffle(_rnd))
+                Console.Write(scan);
+            Console.WriteLine();
+
+            foreach (int scan in iNums)
+                Console.Write(scan + " ");
+            Console.WriteLine();
+            foreach (int scan in iNums.Shuffle(_rnd))
+                Console.Write(scan + " ");
+
+            Console.ReadKey();
+        }
+    }
+}
